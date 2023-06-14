d4294c2bd62ba6a2d4c94609c592cc6120ae56b8
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2023-06-14 11:07:56 -0600

    testing

---
+++ aws/infra/terraform.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.49.0"
+    }
+  }
+  cloud {
+    organization = "samir-playground"
+    workspaces {
+      name = "terraform-eks"
+    }
+  }
+}
